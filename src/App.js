9fa03f7e3ceae53eb37ef4e132fb14a6e79ffd26
louisgrennell <louisgrennell@gmail.com>
2023-06-03 19:07:35 +0100

    New UI for to do list, basic text box and to do list items added for customization and use.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import React, { useState } from 'react';
+
+function TodoForm({ onSubmit, buttonText, initialValue = "", onChange = () => {} }) {
+  const [value, setValue] = useState(initialValue);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(value);
+    setValue("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input className="todo-input" value={value} onChange={(e) => { setValue(e.target.value); onChange(e.target.value); }} />
+      <button className="submit-button" type="submit">{buttonText}</button>
+    </form>
+  );
+}
+
+function TodoItem({ todo, className, onEditClick, onDeleteClick }) {
+  return (
+    <div className={className}>
+      <div className="todo-item-content">
+        <span className="todo-text">{todo}</span>
+        <div>
+          <button className="edit-button" onClick={onEditClick}>Edit</button>
+          <button className="delete-button" onClick={onDeleteClick}>Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+function TodoApp() {
+  const [todos, setTodos] = useState([]);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editText, setEditText] = useState("");
+
+  const handleNewSubmit = (value) => {
+    setTodos([...todos, value]);
+  };
+
+  const handleEditSubmit = (value) => {
+    const newTodos = [...todos];
+    newTodos[editIndex] = value;
+    setTodos(newTodos);
+    setEditIndex(null);
+  };
+
+  const handleDeleteClick = (index) => {
+    const newTodos = todos.filter((_, todoIndex) => todoIndex !== index);
+    setTodos(newTodos);
+    if (editIndex === index) { setEditIndex(null); }
+  };
+
+  let todoItemClass = "todo-item todo-item-large";
+  if (todos.length >= 5) {
+    todoItemClass = "todo-item todo-item-small";
+  } else if (todos.length >= 3) {
+    todoItemClass = "todo-item todo-item-medium";
+  } else {
+    todoItemClass = "todo-item todo-item-large";
+  }
+
+  return (
+    <div className="todo-box">
+      {editIndex === null ? (
+        <TodoForm onSubmit={handleNewSubmit} buttonText="Add" />
+      ) : (
+        <TodoForm
+          onSubmit={handleEditSubmit}
+          buttonText="Save"
+          initialValue={editText}
+          onChange={setEditText}
+        />
+      )}
+
+      <div className={`todo-list ${todoItemClass}`}>
+        {todos.map((todo, index) =>
+          index === editIndex ? (
+            <div key={index}>{editText}</div>
+          ) : (
+           <TodoItem
+             key={index}
+             todo={todo}
+             className={todoItemClass}
+             onEditClick={() => { setEditIndex(index); setEditText(todo); }}
+             onDeleteClick={() => handleDeleteClick(index)}
+           />
+          )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default TodoApp;
+
+
